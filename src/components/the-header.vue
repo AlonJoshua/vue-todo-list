d61ea022d6f38fc16dd5c1d01ee85b193da87904
Alon Joshua <alonjoshua@gmail.com>
2020-12-28 13:02:38 +0200

    clean project, redesign to trello style

---
+++ src/components/the-header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header">
+    
+      <div class="header__top">
+        <span class="header__top--logo">
+          <h1 class="header__top--logo-text">Get Things Done</h1>
+        </span>
+      </div>
+
+      <div class="header__bottom">
+        <div class="btn btn__boards-view">
+          <span class="btn__boards-view--text">
+            Boards
+          </span>
+          <img src="../assets/drop-down-arrow.svg" alt="" class="btn__boards-view--dropdown">
+        </div>
+
+      </div>
+
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
